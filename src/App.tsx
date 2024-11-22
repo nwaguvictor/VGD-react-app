2c09610a6212565a44319944500d2c49e3441c3e
Nwagu Victor <nwaguvictor100@gmail.com>
2024-11-22 13:50:41 +0100

    Rre-updated the chakra setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
-  return <Button colorPalette={'pink'}>Click me</Button>
+  return (
+    <Grid
+      h="200px"
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"red"}></GridItem>
+      <Show when="lg">
+        <GridItem area={"aside"} bg={"orange"}></GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"blue"}></GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from '@/components/ui/provider'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
